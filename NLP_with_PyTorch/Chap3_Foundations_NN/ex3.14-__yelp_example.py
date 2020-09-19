5786588cfdee3bbf8eeac4293e196871259ba806
Phillip Labuschagne <philliplab@gmail.com>
2020-09-19 08:33:50 -0400

    first vectorizer

--- NLP_with_PyTorch/Chap3_Foundations_NN/ex3.14-__yelp_example.py
+++ NLP_with_PyTorch/Chap3_Foundations_NN/ex3.14-__yelp_example.py
@@ -123,6 +123,13 @@ class Vocabulary(object):
         Returns:
             index (int): the integer corresponding to the token
         """
+        if token in self._token_to_idx:
+            index = self._token_to_idx[token]
+        else:
+            index = len(self._token_to_idx)
+            self._token_to_idx[token] = index
+            self._idx_to_token[index] = token
+        return index
 
     def lookup_token(self, token):
         """Retrieve the index associated with the token or the UNK index if token isn't present
@@ -134,7 +141,7 @@ class Vocabulary(object):
         Notes:
             `unk_index` needs to be >= 0 (having been added into the Vocabulary) for the UNK functionality
         """
-        if self.add_unk:
+        if self.unk_indx >= 0:
             return self._token_to_idx.get(token, self.unk_index)
         else:
             return self._token_to_idx[token]
@@ -158,3 +165,87 @@ class Vocabulary(object):
 
     def __len__(self):
         return len(self._token_to_idx)
+
+class ReviewVectorizer(object):
+    """ The Vectorizer which coordinates the Vocabularies and puts them to use"""
+    def __init__(self, review_vocab, rating_vocab):
+        """
+        Args:
+            review_vocab (Vocabulary): maps words to integers
+            rating_vocab (Vocabulary): maps class to integers
+        """
+        self.review_vocab = review_vocab
+        self.rating_vocab = rating_vocab
+
+    def vectorize(self, review):
+        """Create a collapsed one-hot vector for the review
+
+        Args:
+            review (str): the review
+        Returns:
+            one_hot (np.ndarray): the collapsed one-hot encoding
+        """
+        one_hot = np.zeros(len(self.review_vocab), dtype=np.float32)
+
+        for token in review.split(" "):
+            if token not in string.punctuation:
+                one_hot[self.review_vocab.lookup_token(token)] = 1
+
+        return one_hot
+
+    @classmethod
+    def from_dataframe(cls, review_df, cutoff=25):
+        """Instantiate the vectorizer from the dataset dataframe
+
+        Args:
+            review_df (pandas.DataFrame): the review dataset
+            cutoff (int): the parameter for frequency-based filtering
+        Returns:
+            an instance of the ReviewVectorizer
+        """
+
+        review_vocab = Vocabulary(add_unk = True)
+        rating_vocab = Vocabulary(add_unk = False)
+
+        # Add ratings
+        for rating in sorted(set(review_df.rating)):
+            rating_vocab.add_token(rating)
+
+        # Add top words if count > provided count
+        word_counts = Counter()
+        for review in review_df.review:
+            for word in review.split(" "):
+                if word not in string.punctuation:
+                    word_counts += 1
+        
+        for word, count in word_counts.items():
+            if count > cutoff:
+                review_vocab.add_token(word)
+
+        return cls(review_vocab, rating_vocab)
+
+    @classmethod
+    def from_serializable(cls, contents):
+        """Instantiate a ReviewVectorizer from a serializable dictionary
+
+        Args:
+            contents (dict): the serializable dictionary
+        Returns:
+            an instance of the ReviewVectorizer class
+        """
+        review_vocab = Vocabulary.from_serializable(contents['review_vocab'])
+        rating_vocab = Vocabulary.from_serializable(contents['rating_vocab'])
+
+        return cls(review_vocab = review_vocab, rating_vocab = rating_vocab)
+
+    def to_serializable(self):
+        """Create the serializable dictionary for caching
+
+        Returns:
+            contents (dict): the serializable dictionary
+        """
+        return {'review_vocab': self.review_vocab.to_serializable(),
+                'rating_vocab': self.rating_vocab.to_serializable()}
+
+
+
