09a30e942e53ead5fca7929cb0aa557f814589a5
Phillip Labuschagne <philliplab@gmail.com>
2020-09-22 13:34:59 -0400

    fcc pyto cnn data loaders

--- fcc_jovialml/pyto_cifar10_cnn.py
+++ fcc_jovialml/pyto_cifar10_cnn.py
@@ -8,6 +8,9 @@ import pandas as pd
 from torchvision.datasets.utils import download_url
 from torchvision.datasets import ImageFolder
 from torchvision.transforms import ToTensor
+from torch.utils.data.sampler import SubsetRandomSampler
+from torch.utils.data.dataloader import DataLoader
+from torchvision.utils import make_grid
 
 import glob
 import os
@@ -18,6 +21,8 @@ ipython = IPython.get_ipython()
 ipython.magic('matplotlib')
 
 base_dir = '/home/phillipl/0_para/3_resources/PyTorch'
+val_pct = 0.2
+batch_size = 100
 
 # Get data (and check)
 # dataset_url = 'https://files.fast.ai/data/examples/cifar10.tgz'
@@ -39,6 +44,41 @@ def show_example(img, label):
     plt.imshow(img.permute(1, 2, 0))
 
 show_example(*dataset[1000])
+
+def split_indices(n, val_pct = 0.1):
+    indexes = np.random.permutation(n)
+    n_val = int(n * val_pct)
+    val_idxs, train_idxs = indexes[:n_val], indexes[n_val:]
+    return train_idxs, val_idxs
+
+train_idxs, val_idxs = split_indices(len(dataset), val_pct)
+
+train_sampler = SubsetRandomSampler(train_idxs)
+train_dl = DataLoader(dataset = dataset,
+                      batch_size = batch_size,
+                      sampler = train_sampler)
+
+valid_sampler = SubsetRandomSampler(val_idxs)
+valid_dl = DataLoader(dataset = dataset,
+                      batch_size = batch_size,
+                      sampler = valid_sampler)
+
+def show_batch(dl):
+    for images, labels in dl:
+        fig, ax = plt.subplots(figsize = (10, 10))
+        ax.set_xticks([]); ax.set_yticks([])
+        ax.imshow(make_grid(images, 10).permute(1, 2, 0))
+        break
+
+#show_batch(train_dl)
+#show_batch(valid_dl)
+
+
+
+
+
+
+
 
 
 
